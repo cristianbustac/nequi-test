90d3997676c2a3d36443d847b7a02bb4e0bf6c32
cristian andres bustamante <cristianxdanbuxd@gmail.com>
2025-02-25 09:04:49 -0500

    :hammer: initial configuration added

---
+++ src/test/java/com/cristian/springcloud/msvc/prueba/msvc_prueba/MsvcPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.springcloud.msvc.prueba.msvc_prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
